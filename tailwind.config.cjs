f9aa264599d07bf225c3d566ed1da9fa62ed3900
ley0479 <116797427+ley0479@users.noreply.github.com>
2026-08-24 18:14:47 -0500

    fix_local_tailwind_styles

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './frontend/index.html',
+    './frontend/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
